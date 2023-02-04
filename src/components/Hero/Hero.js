3d0ad596bc78d155053877f3d704d53c7a4d3251
Sven Müller <sv-mueller@hotmail.de>
2023-02-04 12:09:58 +0100

    Exercise 2: Improve images, Exercise 3: Accessibility issues

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,42 +1,50 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
+import { createAvifSrcSet, createJpgSrcSet } from "../../data";
 
 const Hero = () => {
-  return (
-    <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
-      <Swoop src="/swoop.svg" />
-    </Wrapper>
-  );
+	const heroSrc = "/images/hero-img.jpg";
+	return (
+		<Wrapper>
+			<HeroPicture>
+				<source type="image/avif" srcSet={createAvifSrcSet(heroSrc)} />
+				<source type="image/jpg" srcSet={createJpgSrcSet(heroSrc)} />
+				<HeroImage src={heroSrc} alt="Kitten hero" />
+			</HeroPicture>
+			<Swoop src="/swoop.svg" alt="" />
+		</Wrapper>
+	);
 };
 
 const Wrapper = styled.section`
-  position: relative;
-  height: 60vh;
-  min-height: 400px;
-  display: flex;
-  justify-content: center;
-  align-items: flex-end;
-  background: hsl(0deg 0% 1%);
+	position: relative;
+	height: 60vh;
+	min-height: 400px;
+	display: flex;
+	justify-content: center;
+	align-items: flex-end;
+	background: hsl(0deg 0% 1%);
 `;
 
+const HeroPicture = styled.picture``;
+
 const HeroImage = styled.img`
-  display: block;
-  width: 500px;
-  height: 500px;
-  max-height: 100%;
+	display: block;
+	width: 500px;
+	height: 500px;
+	max-height: 100%;
 `;
 
 const Swoop = styled.img`
-  position: absolute;
-  left: 0;
-  right: 0;
-  /*
+	position: absolute;
+	left: 0;
+	right: 0;
+	/*
     Overhang by a couple px to prevent any pixel-rounding
     display issues
   */
-  bottom: -2px;
-  width: 100%;
+	bottom: -2px;
+	width: 100%;
 `;
 
 export default Hero;
--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -1,47 +1,55 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
+import { createAvifSrcSet, createJpgSrcSet } from "../../data";
 
 const PhotoGridItem = ({ id, src, alt, tags }) => {
-  return (
-    <article>
-      <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
-      </Anchor>
-      <Tags>
-        {tags.map((tag) => (
-          <Tag key={tag}>{tag}</Tag>
-        ))}
-      </Tags>
-    </article>
-  );
+	return (
+		<article>
+			<Anchor href={`/photos/${id}`}>
+				<Picture>
+					<source type="image/avif" srcSet={createAvifSrcSet(src)} />
+					<source type="image/jpg" srcSet={createJpgSrcSet(src)} />
+					<Image src={src} alt={alt} />
+				</Picture>
+			</Anchor>
+			<Tags>
+				{tags.map((tag) => (
+					<Tag key={tag}>{tag}</Tag>
+				))}
+			</Tags>
+		</article>
+	);
 };
 
 const Anchor = styled.a`
-  text-decoration: none;
-  color: inherit;
-  outline-offset: 4px;
+	text-decoration: none;
+	color: inherit;
+	outline-offset: 4px;
 `;
 
+const Picture = styled.picture``;
+
 const Image = styled.img`
-  display: block;
-  width: 100%;
-  height: 300px;
-  border-radius: 2px;
-  margin-bottom: 8px;
+	display: block;
+	width: 100%;
+	height: 300px;
+	border-radius: 2px;
+	margin-bottom: 8px;
+	object-fit: cover;
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+	display: flex;
+	flex-wrap: wrap;
+	gap: 8px;
 `;
 
 const Tag = styled.li`
-  padding: 4px 8px;
-  background: var(--color-gray-300);
-  font-size: 0.875rem;
-  font-weight: 475;
-  color: var(--color-gray-800);
+	padding: 4px 8px;
+	background: var(--color-gray-300);
+	font-size: 0.875rem;
+	font-weight: 475;
+	color: var(--color-gray-800);
 `;
 
 export default PhotoGridItem;
